2797e3e32cf3dda50686e1e0a7e3f313e525dca7
IswaryaUppliappan <107948265+IswaryaUppliappan@users.noreply.github.com>
2022-11-15 16:27:35 +0530

    Add files via upload

---
+++ week3/day1/org/system/Computer.java
@@ -0,0 +1,8 @@
+package org.system;
+
+public class Computer extends Desktop {
+	public void computerModel() {
+		
+	}
+
+}
---
+++ week3/day1/org/system/Desktop.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class Desktop  {
+	
+	public void desktopSize()
+ {
+		
+	}
+	public static void main(String[] args) {
+		Computer obj=new Computer();
+		obj.computerModel();
+		obj.desktopSize();
+	}
+}
